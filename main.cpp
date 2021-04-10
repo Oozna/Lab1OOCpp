79aeea88079145d5f59c2f3e8fe4a224e17cbc2a
Oozna <johanssonericwilliam@gmail.com>
2021-04-10 16:12:44 +0200

    Structer gjorda

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+
+struct Adress{
+	std::string street;
+	std::string zip;
+	std::string city;
+};
+struct Person{
+	std::string name;
+	std::string id;
+	Adress adress;
+};
+
+int main (){
+}
