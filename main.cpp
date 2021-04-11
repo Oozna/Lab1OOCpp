1e96dcf0c62c667c35b2c28247364f4d260a129f
Oozna <johanssonericwilliam@gmail.com>
2021-04-11 10:51:00 +0200

    funktionsöverlagrat ifstream till structen

--- main.cpp
+++ main.cpp
@@ -13,5 +13,19 @@ struct Person{
 	Adress adress;
 };
 
+std::istream& operator>>(std::istream &in, Person &person) {
+	std::string temp; //to fix spaces
+	std::getline(in, person.name);
+	std::getline(in, person.id);
+	std::getline(in, person.adress.street, ',');
+	std::getline(in, temp, ' '); // removes space before the zip
+	std::getline(in, person.adress.zip, ' '); //first part of zip
+	std::getline(in, temp, ' ');//second part of zip
+	person.adress.zip = (person.adress.zip + " " + temp);//concatonate the zip
+	std::getline(in, temp, ' ');//removes space
+	std::getline(in, person.adress.city);
+	return in;
+}
 int main (){
+
 }
