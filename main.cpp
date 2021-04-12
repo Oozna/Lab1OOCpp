9e25b10549a8057659afa3a06c46d057740e32d6
Oozna <johanssonericwilliam@gmail.com>
2021-04-12 09:29:22 +0200

    read_file funktionen tillagd

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string> 
+#include <vector>
 
 struct Adress{
 	std::string street;
 	std::string zip;
 	std::string city;
 };
-struct Person{
+struct person{
 	std::string name;
 	std::string id;
 	Adress adress;
 };
 
-std::istream& operator>>(std::istream &in, Person &person) {
+std::istream& operator>>(std::istream &in, person &person) {
 	std::string temp; //to fix spaces
 	std::getline(in, person.name);
 	std::getline(in, person.id);
@@ -26,6 +27,22 @@ std::istream& operator>>(std::istream &in, Person &person) {
 	std::getline(in, person.adress.city);
 	return in;
 }
-int main (){
 
+std::vector<person> read_file(std::string filename){
+	person dummy;
+	std::vector<person> list;
+	std::ifstream input(filename);
+	if (input.is_open()){
+		while (input >> dummy){
+			list.push_back(dummy);
+		}
+	}
+	else{
+		std::cout << "Something is wrong with your file bro" << std::endl;
+	}
+	return list;
+}
+
+int main (){
+	std::cout << "Hello man";
 }
